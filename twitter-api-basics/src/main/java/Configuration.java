f943280b41aec612e2c1e00f979abe6f3f1c92ce
Thyago Mota <motat@moravian.edu>
2017-05-24 15:47:48 -0400

    Latest updates

--- twitter-api-basics/src/main/java/Configuration.java
+++ twitter-api-basics/src/main/java/Configuration.java
@@ -1,5 +1,15 @@
-/**
- * Created by meotm01 on 5/24/17.
+/*
+ * SOAR Project Fall 2017
+ * Advisor: Thyago Mota
+ * Student:
+ * Description: Basic configuration settings
  */
+
 public class Configuration {
+
+    public static final String CONSUMER_KEY         = "";
+    public static final String CONSUMER_SECRET      = "";
+    public static final String ACCESS_TOKEN_KEY     = "";
+    public static final String ACCESS_TOKEN_SECRET  = "";
+
 }
