98b51ea2cd801c6dfd62f8f174ac7d4f639b972f
Thyago Mota <motat@moravian.edu>
2017-05-25 09:11:43 -0400

    Latest updates

---
+++ mongodb-basics/src/main/java/Configuration.java
@@ -0,0 +1,13 @@
+/*
+ * SOAR Project Fall 2017
+ * Advisor: Thyago Mota
+ * Student:
+ * Description: Basic configuration settings
+ */
+
+public class Configuration {
+
+    public static final String DB_SERVER = "localhost";
+    public static final int    DB_PORT   = 27017;
+    public static final String DB_NAME   = "soarf17";
+}
