03639ec1e66d9dc5c66021984f8d09afadef7dc0
Thyago Mota <motat@moravian.edu>
2017-08-29 14:37:31 -0400

    Latest updates

---
+++ main/src/main/java/FriendsCrawler.java
@@ -0,0 +1,13 @@
+/*
+ * SOAR Project Fall 2017
+ * Advisor: Thyago Mota
+ * Student: Zachary Balga
+ * Description: a crawler script that will implement a BFS (Breadth First Search) algorithm to sample the friendship network of a given Twitter user up to a pre-defined friendship distance
+ */
+
+public class FriendsCrawler {
+
+    public static void main(String args[]) {
+        System.out.println("It works!");
+    }
+}
